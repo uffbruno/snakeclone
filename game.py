00ed1824e5d3f595b01b3dec1fc028d451b7a86c
Bruno Rabello de Oliveira <uffbruno@gmail.com>
2023-11-04 10:07:08 -0300

    Move rectangle on screen using WASD keys. Solves #2

--- game.py
+++ game.py
@@ -8,6 +8,9 @@ class Game:
         screen = pygame.display.set_mode((800, 600))
         clock = pygame.time.Clock()
         running = True
+        dt = 0
+
+        player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
         while running:
             for event in pygame.event.get():
@@ -15,9 +18,23 @@ class Game:
                     running = False
 
             screen.fill("palegreen2")
-            pygame.draw.rect(screen, "white", (32, 32, 32, 32))
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_ESCAPE]:
+                running = False
+            if keys[pygame.K_w]:
+                player_pos.y -= 100 * dt
+            if keys[pygame.K_a]:
+                player_pos.x -= 100 * dt
+            if keys[pygame.K_s]:
+                player_pos.y += 100 * dt
+            if keys[pygame.K_d]:
+                player_pos.x += 100 * dt
+
+            pygame.draw.rect(screen, "white", (player_pos.x, player_pos.y, 40, 40))
 
             pygame.display.flip()
-            clock.tick(60)
+            dt = clock.tick(60) / 1000
 
         pygame.quit()
