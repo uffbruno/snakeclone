114e2d4d7d105743fe7a0cf964e8c75abeb77fd9
Bruno Rabello de Oliveira <uffbruno@gmail.com>
2025-04-03 14:08:49 -0300

    Improve current existing game over screen. Solves #16

--- gameoverscreen.py
+++ gameoverscreen.py
@@ -1,10 +1,11 @@
+import os
 import pygame
 from gamescreen import gamescreen, GameState
-
 
 class gameoverscreen(gamescreen):
     def __init__(self, display):
         self.screen = display
+        self.bg_image = pygame.image.load(os.path.join('assets', 'images', 'game-over.png'))
         self.font = pygame.font.SysFont("Comic Sans MS", 30)
 
     def update(self) -> GameState:
@@ -12,5 +13,4 @@ class gameoverscreen(gamescreen):
         return GameState.NO_CHANGE
 
     def draw(self):
-        text_display = self.font.render("GAME OVER", False, (0, 0, 0))
-        self.screen.blit(text_display, (320, 320))
+        self.screen.blit(self.bg_image, (0, 0))
